94c3ff3989e34e4738c1b3014e5bf462f8927b0c
Shish <shish@shishnet.org>
2013-10-13 15:14:05 +0100

    image upload test

--- turnabout/tests/test_attachment.py
+++ turnabout/tests/test_attachment.py
@@ -7,9 +7,16 @@ from turnabout.tests import TurnaboutTest
 from turnabout.views import attachment_create, attachment_read, attachment_delete
 from turnabout.views import story_read
 
+png1x1 = (
+    '\x89PNG\r\n\x1a\n'
+    '\x00\x00\x00\rIHDR\x00\x00\x00\x01\x00\x00\x00\x01\x08\x00\x00\x00\x00:~\x9bU'
+    '\x00\x00\x00\nIDAT\x08\xd7c\xf8\x0f\x00\x01\x01\x01\x00\x1b\xb6\xeeV'
+    '\x00\x00\x00\x00IEND\xaeB`\x82\n'
+)
+
 
 class TestAttachmentCreate(TurnaboutTest):
-    def test_pass(self):
+    def test_pass_plain(self):
         request = testing.DummyRequest(
             matchdict={"tracker_id": "1", "story_id": "1"},
             POST={
@@ -28,6 +35,26 @@ class TestAttachmentCreate(TurnaboutTest):
         story = story_read(request)
         self.assertEqual(story.attachments[-1].filename, u"test.dat")
         self.assertEqual(story.attachments[-1].hash, "916f0027a575074ce72a331777c3478d6513f786a591bd892da1a577bf2335f9")
+
+    def test_pass_image(self):
+        request = testing.DummyRequest(
+            matchdict={"tracker_id": "1", "story_id": "1"},
+            POST={
+                "file": Mock(
+                    filename=u"1x1.png",
+                    file=StringIO(png1x1),
+                    type="image/png",
+                ),
+            },
+            user=self.user
+        )
+        resp = attachment_create(request)
+        self.assertEqual(resp.status, "302 Found")
+
+        request = testing.DummyRequest(matchdict={"tracker_id": "1", "story_id": "1"})
+        story = story_read(request)
+        self.assertEqual(story.attachments[-1].filename, u"1x1.png")
+        self.assertEqual(story.attachments[-1].hash, "119dfc24dc533791fb48a646096b3f191fef420c934e25dcb61d036a5554e211")
 
 
 class TestAttachmentRead(TurnaboutTest):
